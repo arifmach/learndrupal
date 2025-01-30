172d8bcd6f26d3e53c89bd55b95467f18eee4439
Mohd Arif <mohammadarifaligarh@gmail.com>
2025-01-30 20:12:40 +0400

    Initial Commit for Code

---
+++ web/themes/custom/yummyred/js/yummyred.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * YummyRed behaviors.
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.yummyred = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+})(jQuery, Drupal);
